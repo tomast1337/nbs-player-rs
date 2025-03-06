bb9c69a22d65aa8c9944e558cb6b26a370d96df7
tomast1337 <vycasnicolas@gmail.com>
2025-03-06 17:03:33 -0300

    Add unit test for NBS file playback and include sample NBS file

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,15 @@ pub fn play_file(nbs_file_data: Vec<u8>) -> String {
     let song_name_bytes = nbs_file.header.song_name;
     str::from_utf8(&song_name_bytes).unwrap().to_string()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn load_nbs_file() {
+        let file_data = include_bytes!("../test-assets/nyan_cat.nbs");
+        let song_name = play_file(file_data.to_vec());
+        assert_eq!(song_name, "Nyan Cat");
+    }
+}
