2f7c7c314525fcf120eeee21a53037b8e6393132
tomast1337 <vycasnicolas@gmail.com>
2025-03-11 19:54:42 -0300

    Refactor PianoKey initialization and update default song file in load_nbs_file function

--- ray-lib/src/song.rs
+++ ray-lib/src/song.rs
@@ -3,7 +3,7 @@ use nbs_rs::{NbsFile, NbsParser};
 
 pub fn load_nbs_file(song_data: Option<Vec<u8>>) -> NbsFile {
     let song_data_bytes = song_data
-        .unwrap_or_else(|| include_bytes!("../test-assets/Note Block Megacollab.nbs").to_vec());
+        .unwrap_or_else(|| include_bytes!("../test-assets/Metropolis of Illusion.nbs").to_vec());
     log::info!("Loading song with {:?} bytes", song_data_bytes.len());
     let mut song_file = NbsParser::new(&song_data_bytes);
     let song = song_file.parse().unwrap();
