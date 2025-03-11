eb596295f566fb1ab93499d4b990d761499e391f
tomast1337 <vycasnicolas@gmail.com>
2025-03-11 19:42:49 -0300

    Add logging functionality and update dependencies for improved traceability

--- ray-lib/src/song.rs
+++ ray-lib/src/song.rs
@@ -1,9 +1,12 @@
+use log;
 use nbs_rs::{NbsFile, NbsParser};
 
 pub fn load_nbs_file(song_data: Option<Vec<u8>>) -> NbsFile {
-    let song_data_bytes =
-        song_data.unwrap_or_else(|| include_bytes!("../test-assets/turkish_march.nbs").to_vec());
+    let song_data_bytes = song_data
+        .unwrap_or_else(|| include_bytes!("../test-assets/Note Block Megacollab.nbs").to_vec());
+    log::info!("Loading song with {:?} bytes", song_data_bytes.len());
     let mut song_file = NbsParser::new(&song_data_bytes);
     let song = song_file.parse().unwrap();
+    log::info!("Loaded song successfully");
     song
 }
