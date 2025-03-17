99ba75ce8df978fba4ebf916adca7eabaa9cf867
tomast1337 <vycasnicolas@gmail.com>
2025-03-17 10:01:51 -0300

    Set up Macroquad configuration for WebGL2 in wasm_bindgen demo

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,29 @@
+use macroquad::{
+    self,
+    miniquad::{
+        conf::{Platform, WebGLVersion},
+        start,
+    },
+};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 pub fn run_demo(demo_data: Vec<u8>, canvas_id: String, window_width: f32, window_height: f32) {
     // execute macroquad on the canvas
+    let conf = macroquad::window::Conf {
+        window_title: "BasicShapes".to_string(),
+        window_width: window_width as i32,
+        window_height: window_height as i32,
+        high_dpi: false,
+        fullscreen: false,
+        sample_count: 1,
+        window_resizable: false,
+        icon: Default::default(),
+        platform: Platform {
+            webgl_version: WebGLVersion::WebGL2,
+            ..Default::default()
+        },
+    };
+
+    //native::wasm::run(&conf, || {});
 }
-
-/*
-Here’s a clearer and more concise version of your message:
-
----
-
-**Title:** Running a Macroquad App in a Web Browser with `wasm_bindgen`
-
-Hi everyone,
-
-
-*/
