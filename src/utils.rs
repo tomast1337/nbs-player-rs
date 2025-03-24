d8e37ab688d34ecc55769943685b93c08b54c84b
tomast1337 <vycasnicolas@gmail.com>
2025-03-23 21:03:04 -0300

    Remove commented-out logger_callback function to clean up code

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,3 @@
-use raylib::ffi::TraceLogLevel;
-
 /// Formats a time in seconds to a string in the format "mm:ss".
 pub fn time_formatter(time: f32) -> String {
     let minutes = (time / 60.0).floor() as u32;
@@ -11,3 +9,18 @@ pub fn time_formatter(time: f32) -> String {
 pub fn lerp(start: f32, end: f32, t: f32) -> f32 {
     start + (end - start) * t
 }
+
+/*
+pub fn logger_callback(level: raylib::ffi::TraceLogLevel, text: &str) {
+    match level {
+        TraceLogLevel::LOG_ALL => log::trace!("{}", text),
+        TraceLogLevel::LOG_TRACE => log::trace!("{}", text),
+        TraceLogLevel::LOG_DEBUG => log::debug!("{}", text),
+        TraceLogLevel::LOG_INFO => log::info!("{}", text),
+        TraceLogLevel::LOG_WARNING => log::warn!("{}", text),
+        TraceLogLevel::LOG_ERROR => log::error!("{}", text),
+        TraceLogLevel::LOG_FATAL => log::error!("{}", text),
+        TraceLogLevel::LOG_NONE => {}
+    }
+}
+*/
